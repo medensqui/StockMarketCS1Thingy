434f22d83ddf86d53f3930dcf24edfe3c8f62387
medensqui <medensqui@noreply.users.github.com>
2016-04-27 09:40:43 -0400

    Add original code (note: to run copy into eclipse)

---
+++ Stock.java
@@ -0,0 +1,23 @@
+import org.dalton.DaltonStock;
+
+/**
+ *
+ */
+
+/**
+ * @author student
+ *
+ */
+public class Stock {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DaltonStock stock = new DaltonStock("AMZN");
+		stock.
+		System.out.println(stock.name);
+	}
+
+}
